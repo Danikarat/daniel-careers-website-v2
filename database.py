62b9b457f05ec566772efc606e889a7cad8605e1
daniel <danieldzotsi24@gmail.com>
2024-06-12 14:18:39 +0000

    still trying to fix the database.py problem of converting the mysql database content to a python dictionary

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text,column, Integer, String
 
 # Replace these variables with your actual database credentials
 username = 'root'
 password = 'danXorse24$'
-host = 'your_public_ip_or_domain'
-port = '3307'
+host = 'localhost'
+port = '3307'  # usually 3306 for MySQL
 database = 'danicareers'
 
 # Create the connection string
@@ -14,11 +14,20 @@ connection_string = f'mysql+mysqlconnector://{username}:{password}@{host}:{port}
 engine = create_engine(connection_string)
 
 # Establish the connection
-try:
-    conn = engine.connect()
-    print("Connection to MySQL DB successful")
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    conn.close()
-    print("Connection closed")
+conn = engine.connect()
+
+# Now you can use 'conn' to interact with the database
+with conn:
+    result = conn.execute(text("select * from jobs"))
+    print("type(result):", type(result))
+    result_all = result.all()
+    print("type(result.all()):", type(result_all))
+    first_result = result_all[0]
+    print("type(first_result):", type(first_result))
+    
+    #create a dixtionary from the first result row
+    first_result_dict = dict(first_result)
+    print("type(first_result_dict):", type(first_result_dict))
+    print(first_result_dict)
+    
+    
